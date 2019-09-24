91099d5524d05804417352cf921e83e814c327b2
Alex223124 <sologoubalex@gmail.com>
2019-09-24 20:03:02 +0100

    spec helper updated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'rack/test'
 require 'rspec'
+require 'factory_bot'
+require "./config/environment"
 
 ENV['RACK_ENV'] = 'test'
 
@@ -13,4 +15,10 @@ end
 
 RSpec.configure do |c|
   c.include RSpecMixin
+
+  c.include FactoryBot::Syntax::Methods
+
+  c.before(:suite) do
+    FactoryBot.find_definitions
+  end
 end
