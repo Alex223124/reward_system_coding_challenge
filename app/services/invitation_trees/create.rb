52b67f3665ec0fd10d8a45c708e81921bc13922c
Alex223124 <sologoubalex@gmail.com>
2019-09-24 20:01:49 +0100

    nodes renamed to recommendations

--- app/services/invitation_trees/create.rb
+++ app/services/invitation_trees/create.rb
@@ -2,8 +2,8 @@ class Services::InvitationTrees::Create
 
   attr_reader :tree
 
-  def initialize(nodes)
-    @nodes = nodes
+  def initialize(recommendations)
+    @recommendations = recommendations
   end
 
   def perform
@@ -15,29 +15,29 @@ class Services::InvitationTrees::Create
   private
 
   def root_node
-    @tree = Tree::TreeNode.new(sorted_nodes.first[:sender])
+    @tree = Tree::TreeNode.new(sorted_recommendations.first[:sender])
   end
 
   def add_first_child_node
-    @tree.root << Tree::TreeNode.new(sorted_nodes.first[:receiver])
+    @tree.root << Tree::TreeNode.new(sorted_recommendations.first[:receiver])
   end
 
-  def sorted_nodes
-    @sorted_nodes ||= @nodes.sort_by { |rec| rec[:time] }
+  def sorted_recommendations
+    @sorted_recommendations ||= @recommendations.sort_by { |rec| rec[:time] }
   end
 
   def update
-    sorted_nodes.drop(1).each do |node|
+    sorted_recommendations.drop(1).each do |node|
       insert_node(node)
     end
   end
 
   def insert_node(new_node)
-    last_node = find_last_node_by_sender(new_node)
+    last_node = find_node_by_sender(new_node)
     add_child_to(last_node, new_node)
   end
 
-  def find_last_node_by_sender(node)
+  def find_node_by_sender(node)
     @tree.find { |n| n.name == node[:sender]}
   end
 
